45ab10bc70365aaf2f63a201bbe189acfde841db
elisaverst <lisa.grustnaya@gmail.com>
2026-04-02 21:23:27 +0300

    edit hw #5

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,14 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get('https://www.saucedemo.com/')
+def find_elements():
+    driver = webdriver.Chrome()
+    driver.get('https://www.saucedemo.com/')
 
-username = driver.find_element(By.XPATH, '//*[@id="user-name"]')
-password = driver.find_element(By.XPATH, '//*[@id="password"]')
-login_button = driver.find_element(By.XPATH, '//*[@id="login-button"]')
+    try:
+        username = driver.find_element(By.XPATH, '//*[@id="user-name"]')
+        password = driver.find_element(By.XPATH, '//*[@id="password"]')
+        login_button = driver.find_element(By.XPATH, '//*[@id="login-button"]')
 
-if username is None and password is None and login_button is None:
-    print('Элементы не найдены')
-else:
-    print('Элементы найдены')
+        if username and password and login_button:
+            print(f'Элементы найдены')
+
+    except:
+        print(f'Элементы не найдены')
+
+    finally:
+        driver.quit()
+
+find_elements()
